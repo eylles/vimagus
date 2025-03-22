a391783bc54500d6d6a529483855f7d7330c5fc8
eylles <ed.ylles1997@gmail.com>
2025-03-22 12:28:03 -0600

    Update comments

--- lua/core/pconf.lua
+++ lua/core/pconf.lua
@@ -289,15 +289,13 @@ cmp.setup({
   }
 })
 
----------------
--- nvim-tree --
----------------
+--------------
+-- neo-tree --
+--------------
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- empty setup using defaults
--- require("nvim-tree").setup()
 require("neo-tree").setup()
 
 -------------
