ac8e3c109d7de56b1ea8bc3c0a3226893af7cdea
eylles <ed.ylles1997@gmail.com>
2026-01-01 18:55:27 -0600

    Map leader and localleader before plugins
    As recommended, by documentation map these before plugins are even loaded.

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,8 @@
 vim.cmd('source ~/.config/nvim/lua/core/plug-bootstrap.vim')
+
+vim.g.mapleader = ","
+vim.g.maplocalleader = "-"
+
 require ('core.plug')
 require ('core.set')
 require ('core.pconf')
