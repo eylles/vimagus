953e560da011a8cb4c17ec3fc94d208b34a56b4c
eylles <ed.ylles1997@gmail.com>
2026-01-17 17:50:03 -0600

    Ditch mini.notify and mini.cmdline for noice

--- lua/core/pconf.lua
+++ lua/core/pconf.lua
@@ -120,11 +120,6 @@ require('mini.fuzzy').setup()
 --------------
 require('hlsearch').setup()
 
--------------------
--- notifications --
--------------------
-require('mini.notify').setup()
-
 ----------------
 -- cursorword --
 ----------------
@@ -163,9 +158,47 @@ require('mini.bracketed').setup()
 ------------------
 -- command line --
 ------------------
-require('mini.cmdline').setup()
-
 require("noice").setup({
+  cmdline = {
+    enabled = true, -- enables the Noice cmdline UI
+    view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+    opts = {}, -- global options for the cmdline. See section on views
+    ---@type table<string, CmdlineFormat>
+    format = {
+      -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+      -- view: (default is cmdline view)
+      -- opts: any options passed to the view
+      -- icon_hl_group: optional hl_group for the icon
+      -- title: set to anything or empty string to hide
+      cmdline = { pattern = "^:", icon = "", lang = "vim" },
+      search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+      filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+      lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+      help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+      input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+      -- lua = false, -- to disable a format, set to `false`
+    },
+  },
+  messages = {
+    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+    -- This is a current Neovim limitation.
+    enabled = true, -- enables the Noice messages UI
+    view = "notify", -- default view for messages
+    view_error = "notify", -- view for errors
+    view_warn = "notify", -- view for warnings
+    view_history = "messages", -- view for :messages
+    view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+  },
+  notify = {
+    -- Noice can be used as `vim.notify` so you can route any notification like other messages
+    -- Notification messages have their level and other properties set.
+    -- event is always "notify" and kind can be any log level as a string
+    -- The default routes will forward notifications to nvim-notify
+    -- Benefit of using Noice for this is the routing and consistent history view
+    enabled = true,
+    view = "notify",
+  },
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
