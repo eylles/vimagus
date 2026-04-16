3f01cf1c7f0bacd08f3dfd62fd6219d114ccc80d
eylles <ed.ylles1997@gmail.com>
2026-04-16 08:19:59 -0600

    Add ada to list of treesitter parsers

--- lua/core/pconf.lua
+++ lua/core/pconf.lua
@@ -308,7 +308,7 @@ require('mini.bufremove').setup()
 require('nvim-treesitter.configs').setup({
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = {
-    "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rasi", "python"
+    "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rasi", "python", "ada"
   },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
