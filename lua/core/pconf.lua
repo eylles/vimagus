33b4c5bd77830f207a3f1391a8f56dfdbf346459
eylles <ed.ylles1997@gmail.com>
2026-04-17 14:17:37 -0600

    Move initialization of noice around
    This ought to make noice play nicer with cmp

--- lua/core/pconf.lua
+++ lua/core/pconf.lua
@@ -155,116 +155,6 @@ require('mini.operators').setup()
 ----------------
 require('mini.bracketed').setup()
 
-------------------
--- command line --
-------------------
-require("noice").setup({
-  cmdline = {
-    enabled = true, -- enables the Noice cmdline UI
-    view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-    opts = {}, -- global options for the cmdline. See section on views
-    ---@type table<string, CmdlineFormat>
-    format = {
-      -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-      -- view: (default is cmdline view)
-      -- opts: any options passed to the view
-      -- icon_hl_group: optional hl_group for the icon
-      -- title: set to anything or empty string to hide
-      cmdline = { pattern = "^:", icon = "", lang = "vim", conceal = false },
-      search_down = { kind = "search", pattern = "^/", lang = "regex" },
-      search_up = { kind = "search", pattern = "^%?", lang = "regex" },
-      filter = { pattern = "^:%s*!", lang = "bash", conceal = false, title = "Shell" },
-      lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, lang = "lua", conceal = false },
-      help = { pattern = "^:%s*he?l?p?%s+" },
-      input = { view = "cmdline_input", icon = "> "}, -- Used by input()
-      -- lua = false, -- to disable a format, set to `false`
-    },
-  },
-  messages = {
-    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-    -- This is a current Neovim limitation.
-    enabled = true, -- enables the Noice messages UI
-    view = "notify", -- default view for messages
-    view_error = "notify", -- view for errors
-    view_warn = "notify", -- view for warnings
-    view_history = "messages", -- view for :messages
-    view_search = "notify", -- view for search count messages. Set to `false` to disable
-  },
-  notify = {
-    -- Noice can be used as `vim.notify` so you can route any notification like other messages
-    -- Notification messages have their level and other properties set.
-    -- event is always "notify" and kind can be any log level as a string
-    -- The default routes will forward notifications to nvim-notify
-    -- Benefit of using Noice for this is the routing and consistent history view
-    enabled = true,
-    view = "notify",
-  },
-  popupmenu = {
-    enabled = true, -- enables the Noice popupmenu UI
-    ---@type 'nui'|'cmp'
-    backend = "nui", -- backend to use to show regular cmdline completions
-    ---@type NoicePopupmenuItemKind|false
-    -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
-    kind_icons = {}, -- set to `false` to disable icons
-  },
-  -- default options for require('noice').redirect
-  -- see the section on Command Redirection
-  ---@type NoiceRouteConfig
-  redirect = {
-    view = "popup",
-    filter = {
-      event = "msg_show",
-    },
-  },
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-    },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = false, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = true, -- add a border to hover docs and signature help
-  },
-  views = {
-    notify = {
-      backend = "notify",
-      replace = true,
-      merge = true,
-    },
-  },
-  routes = {
-    {
-      view = "notify",
-      filter = { event = "msg_showmode" },
-    },
-    {
-      view = 'notify',
-      filter = {
-        event = 'msg_showcmd',
-        any = {
-          { find = '[mq]$' }
-        },
-      },
-    },
-    {
-      view = 'notify',
-      filter = {
-        event = 'msg_showmode',
-        any = {
-          { find = 'recording' },
-        },
-      },
-    },
-  }
-})
-
 --------------------
 -- tmux navigator --
 --------------------
@@ -494,6 +384,116 @@ cmp.setup({
         return vim_item
       end
     })
+  }
+})
+
+------------------
+-- command line --
+------------------
+require("noice").setup({
+  cmdline = {
+    enabled = true, -- enables the Noice cmdline UI
+    view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+    opts = {}, -- global options for the cmdline. See section on views
+    ---@type table<string, CmdlineFormat>
+    format = {
+      -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+      -- view: (default is cmdline view)
+      -- opts: any options passed to the view
+      -- icon_hl_group: optional hl_group for the icon
+      -- title: set to anything or empty string to hide
+      cmdline = { pattern = "^:", icon = "", lang = "vim", conceal = false },
+      search_down = { kind = "search", pattern = "^/", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", lang = "regex" },
+      filter = { pattern = "^:%s*!", lang = "bash", conceal = false, title = "Shell" },
+      lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, lang = "lua", conceal = false },
+      help = { pattern = "^:%s*he?l?p?%s+" },
+      input = { view = "cmdline_input", icon = "> "}, -- Used by input()
+      -- lua = false, -- to disable a format, set to `false`
+    },
+  },
+  messages = {
+    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+    -- This is a current Neovim limitation.
+    enabled = true, -- enables the Noice messages UI
+    view = "notify", -- default view for messages
+    view_error = "notify", -- view for errors
+    view_warn = "notify", -- view for warnings
+    view_history = "messages", -- view for :messages
+    view_search = "notify", -- view for search count messages. Set to `false` to disable
+  },
+  notify = {
+    -- Noice can be used as `vim.notify` so you can route any notification like other messages
+    -- Notification messages have their level and other properties set.
+    -- event is always "notify" and kind can be any log level as a string
+    -- The default routes will forward notifications to nvim-notify
+    -- Benefit of using Noice for this is the routing and consistent history view
+    enabled = true,
+    view = "notify",
+  },
+  popupmenu = {
+    enabled = true, -- enables the Noice popupmenu UI
+    ---@type 'nui'|'cmp'
+    backend = "nui", -- backend to use to show regular cmdline completions
+    ---@type NoicePopupmenuItemKind|false
+    -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
+    kind_icons = {}, -- set to `false` to disable icons
+  },
+  -- default options for require('noice').redirect
+  -- see the section on Command Redirection
+  ---@type NoiceRouteConfig
+  redirect = {
+    view = "popup",
+    filter = {
+      event = "msg_show",
+    },
+  },
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = false, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = true, -- add a border to hover docs and signature help
+  },
+  views = {
+    notify = {
+      backend = "notify",
+      replace = true,
+      merge = true,
+    },
+  },
+  routes = {
+    {
+      view = "notify",
+      filter = { event = "msg_showmode" },
+    },
+    {
+      view = 'notify',
+      filter = {
+        event = 'msg_showcmd',
+        any = {
+          { find = '[mq]$' }
+        },
+      },
+    },
+    {
+      view = 'notify',
+      filter = {
+        event = 'msg_showmode',
+        any = {
+          { find = 'recording' },
+        },
+      },
+    },
   }
 })
 
